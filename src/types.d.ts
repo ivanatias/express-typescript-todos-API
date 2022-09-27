c1152aba25cd5cbbe87b8c531dc51240ba8b28a1
Ivan Atias <ivan.d.atias@gmail.com>
2022-09-26 20:51:44 -0400

    refactor: remove unneeded files

--- src/types.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Todo {
-  id: number
-  date: string
-  title: string
-  isCompleted: boolean
-  isPriority: boolean
-}
-
-export type NewTodoEntry = Omit<Todo, 'id', 'isCompleted'>

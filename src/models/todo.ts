6ce43ce62ea26f2ed7e9ab093718096f478b4ba9
Ivan Atias <ivan.d.atias@gmail.com>
2022-10-02 19:55:38 -0400

    feat: add user field in Todo model

--- src/models/todo.ts
+++ src/models/todo.ts
@@ -1,17 +1,23 @@
 import { Model, Schema, Document, model } from 'mongoose'
+import { UserType } from './user'
 
 export interface TodoType extends Document {
   title: string
   date: Date
   isPriority: boolean
   isCompleted: boolean
+  user: UserType
 }
 
 const todoSchema = new Schema<TodoType, Model<TodoType>>({
   title: { type: String, required: true },
   date: { type: Date, required: true },
   isPriority: { type: Boolean, required: true },
-  isCompleted: { type: Boolean, required: true }
+  isCompleted: { type: Boolean, required: true },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 todoSchema.set('toJSON', {

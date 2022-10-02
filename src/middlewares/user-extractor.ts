9c5ba539f6ef70ba67dfd67052c9a59c4664cf9f
Ivan Atias <ivan.d.atias@gmail.com>
2022-10-02 19:53:55 -0400

    feat: add user extractor middleware

---
+++ src/middlewares/user-extractor.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from 'express'
+import jwt, { JwtPayload } from 'jsonwebtoken'
+
+const JWT_SECRET = process.env.JWT_SECRET
+
+export interface ExtractorRequest extends Request {
+  userId?: string
+}
+
+export interface AuthToken extends JwtPayload {
+  id: string
+  username: string
+}
+
+export default (req: ExtractorRequest, _res: Response, next: NextFunction) => {
+  const auth = req.get('Authorization')
+  let token = ''
+
+  if (auth && auth.startsWith('Bearer' || 'bearer')) {
+    token = auth.substring(7)
+  }
+
+  const decodedToken = jwt.verify(token, JWT_SECRET as string) as AuthToken
+
+  const { id } = decodedToken
+
+  req.userId = id ?? undefined
+
+  next()
+}

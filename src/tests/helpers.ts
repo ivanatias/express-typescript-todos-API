4da907fcd07c9e3584deff6aa253fa3eeb13dc1f
Ivan Atias <ivan.d.atias@gmail.com>
2022-10-05 04:14:53 -0400

    refactor(tests-helpers): replace static id with mongoose new object id

--- src/tests/helpers.ts
+++ src/tests/helpers.ts
@@ -1,5 +1,6 @@
 import { app } from '../index'
 import supertest from 'supertest'
+import { Types } from 'mongoose'
 import jwt from 'jsonwebtoken'
 import { User } from '../models/user'
 
@@ -17,7 +18,7 @@ const userWithTodo = {
   name: 'User with todo',
   username: 'userwithtodo',
   passwordHash: 'test',
-  _id: '633b59a165510680300479be'
+  _id: new Types.ObjectId()
 }
 
 const dummyTodos = [
@@ -32,28 +33,25 @@ const dummyTodos = [
     isPriority: true,
     isCompleted: false,
     date: new Date(),
-    user: '633b59a165510680300479be'
+    user: userWithTodo._id
   },
   {
     title: 'This todo should be updated',
     isPriority: true,
     isCompleted: false,
     date: new Date(),
-    user: '633b59a165510680300479be'
+    user: userWithTodo._id
   },
   {
     title: 'This todo should not be deleted',
     isPriority: true,
     isCompleted: false,
     date: new Date(),
-    user: '633b59a165510680300479be'
+    user: userWithTodo._id
   }
 ]
 
-const provideTokenToUser = (
-  username: string | undefined,
-  id: string | undefined
-) => {
+const provideTokenToUser = (username: string, id: string) => {
   const userDataForToken = {
     username,
     id
@@ -68,7 +66,7 @@ const createAndSaveTestUser = async (
   name: string,
   username: string,
   password: string,
-  id?: string
+  id?: Types.ObjectId
 ) => {
   const newTestUser = new User({
     name,

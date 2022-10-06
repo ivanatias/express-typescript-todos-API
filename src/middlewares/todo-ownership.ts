d2ebe0c2d0c5a65fca3df9e7902e049fd560a695
Ivan Atias <ivan.d.atias@gmail.com>
2022-10-06 19:01:57 -0400

    refactor(todos): abstract todo ownership checking logic into a middleware

---
+++ src/middlewares/todo-ownership.ts
@@ -0,0 +1,27 @@
+import { Response, NextFunction } from 'express'
+import { Todo } from '../models/todo'
+import { ExtractorRequest } from './user-extractor'
+
+export default async (
+  req: ExtractorRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params
+
+  const { userId } = req
+
+  const todoToDelete = await Todo.findById(id)
+
+  if (!todoToDelete) return res.status(404).end()
+
+  const userIdInTodo = todoToDelete.user.toString()
+
+  if (userIdInTodo !== userId) {
+    return res.status(401).send({
+      message: 'You are not authorized to manipulate this todo.'
+    })
+  }
+
+  next()
+}

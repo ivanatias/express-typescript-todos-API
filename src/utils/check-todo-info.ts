0903537064298abfb177f11d432eb230998f715b
Ivan Atias <ivan.d.atias@gmail.com>
2022-10-06 20:53:25 -0400

    style(utils): destructure parameters of valid todo check function

--- src/utils/check-todo-info.ts
+++ src/utils/check-todo-info.ts
@@ -1,13 +1,5 @@
 import { isString, isBoolean } from './simple-validators'
 
-export const isValidTodo = (todoFromRequest: any): boolean => {
-  const { title, isPriority } = todoFromRequest
-
+export const isValidTodo = ({ title, isPriority }: any): boolean => {
   return isString(title) && isBoolean(isPriority)
-}
-
-export const isNewTodoInfoValid = (newTodoInfoFromRequest: any): boolean => {
-  const { isCompleted } = newTodoInfoFromRequest
-
-  return isValidTodo(newTodoInfoFromRequest) && isBoolean(isCompleted)
 }

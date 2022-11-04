72d4396bf8ddecba7d5cff41b4c6dac217bdc5a6
Ivan Atias <ivan.d.atias@gmail.com>
2022-11-04 13:07:57 -0400

    refactor: improve type assertion on env variables

--- src/mongo.ts
+++ src/mongo.ts
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose'
+import { assertNonNullable } from './utils/simple-validators'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 const { NODE_ENV, MONGODB_URI, MONGODB_URI_TEST } = process.env
+const VALID_MONGODB_URI = assertNonNullable<string>(MONGODB_URI)
+const VALID_MONGODB_URI_TEST = assertNonNullable<string>(MONGODB_URI_TEST)
 
 const connectionString =
-  NODE_ENV === 'test' ? (MONGODB_URI_TEST as string) : (MONGODB_URI as string)
+  NODE_ENV === 'test' ? VALID_MONGODB_URI_TEST : VALID_MONGODB_URI
 
 const connectDB = () => {
   mongoose
--- src/utils/simple-validators.ts
+++ src/utils/simple-validators.ts
@@ -13,3 +13,11 @@ export const isBoolean = (boolean: boolean): boolean => {
 export const isDate = (date: string): boolean => {
   return Boolean(Date.parse(date))
 }
+
+export const assertNonNullable = <T>(value: T | null | undefined) => {
+  if (value === null || value === undefined) {
+    throw new Error(`Variable cannot be ${String(value)}`)
+  }
+
+  return value
+}

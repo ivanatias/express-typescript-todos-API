d602c1672c96c1475ccc4317fad5bdde090dfbc3
Ivan Atias <ivan.d.atias@gmail.com>
2023-01-11 04:55:55 -0400

    tests: ignore build folder

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
   transform: {
     '^.+\\.ts?$': 'ts-jest'
   },
-  transformIgnorePatterns: ['<rootDir>/node_modules/']
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  modulePathIgnorePatterns: ['<rootDir>/build/']
 }
